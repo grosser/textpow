dca20e348a749f8ac4b11d7b9f1900091ed81814
grosser <grosser.michael@gmail.com>
2011-10-05 20:28:40 +0200

    add benchmark as baseline for improvements

---
+++ examples/benchmark_js.rb
@@ -0,0 +1,11 @@
+$LOAD_PATH.unshift 'lib'
+require 'textpow'
+
+syntax = Textpow::SyntaxNode.load("#{Textpow.syntax_path}/javascript.syntax")
+text = File.read('examples/jquery.js')
+processor = Textpow::RecordingProcessor.new
+
+start = Time.now.to_f
+5.times{ syntax.parse(text,  processor) }
+puts Time.now.to_f - start
+
