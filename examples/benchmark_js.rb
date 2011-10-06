bd6d09092b76dd7d849c6240ebc81f349aac4e3c
grosser <grosser.michael@gmail.com>
2011-10-06 15:51:56 +0200

    make benchmark script adjustable

--- examples/benchmark_js.rb
+++ examples/benchmark_js.rb
@@ -1,3 +1,5 @@
+# test changes for performance
+# ruby examples/benchmark_js.rb 100; git co lib/textpow/syntax.rb ; ruby examples/benchmark_js.rb 100
 $LOAD_PATH.unshift 'lib'
 require 'textpow'
 
@@ -6,6 +8,6 @@ text = File.read('examples/jquery.js')
 processor = Textpow::RecordingProcessor.new
 
 start = Time.now.to_f
-1.times{ syntax.parse(text,  processor) }
+(ARGV[0] || 1).to_i.times{ syntax.parse(text,  processor) }
 puts Time.now.to_f - start
 
